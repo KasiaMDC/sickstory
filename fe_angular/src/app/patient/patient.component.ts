67f5746b93f37aa79b5f154f6f2b4da8c425feae
Katarzyna Monteiro de Carvalho <siostra4@hotmail.com>
2023-10-09 21:52:58 +0200

    delete patient

--- fe_angular/src/app/patient/patient.component.ts
+++ fe_angular/src/app/patient/patient.component.ts
@@ -16,6 +16,7 @@ export class PatientComponent {
 
   constructor(
       private router: Router,
+      private loginStorageService: LoginStorageService,
   ) { }
 
   toggleExpanded(): void {
@@ -25,6 +26,32 @@ export class PatientComponent {
   goToEditPatient(): void {
     this.router.navigate(['/patient'], { queryParams: { id: this.patient.id } });
 
+  }
+
+  deletePatient(): void {
+
+    const authenticationHeader: string = this.loginStorageService.getValue()!;
+    const deletePatientUrl: string = `http://localhost:8080/patient/${this.patient.id}`;
+
+    // Process the first data or trigger the second fetch based on the first data
+    const customHeaders = {
+      'Authorization': authenticationHeader
+    };
+
+    const fetchConfig: RequestInit = {
+      method: 'DELETE',
+      headers: new Headers(customHeaders),
+      //credentials: 'include'
+    };
+
+    fetch(deletePatientUrl, fetchConfig)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Could not delete patient');
+          }
+          alert("Patient deleted successfully!");
+          this.router.navigate(['/']);
+        })
   }
 
 }
